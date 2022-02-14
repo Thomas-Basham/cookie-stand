729f93e9a024d635c56ee061c9860f8631ee0cd5
Thomas Basham <Bashamtg@gmail.com>
2022-02-14 14:49:58 -0800

    Update app.js
    Entered city objects

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,37 @@
 'use strict';
+
+
+let seattle {
+  name: "Seattle"
+  minCustomers:23
+  maxCustomers:65
+  avgCookiePerSale:6.3
+}
+
+let tokyo {
+  name: "Tokyo"
+  minCustomers:3
+  maxCustomers:24
+  avgCookiePerSale:1.2
+}
+
+let dubai {
+  name: "Dubai"
+  minCustomers:11
+  maxCustomers:38
+  avgCookiePerSale:3.7
+}
+
+let Paris {
+  name: "Paris"
+  minCustomers:20
+  maxCustomers:38
+  avgCookiePerSale:2.3
+}
+
+let Lima {
+  name: "Lima"
+  minCustomers:2
+  maxCustomers:16
+  avgCookiePerSale:4.6
+}
