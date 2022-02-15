15df8cc5d12d8cf97208a28d82171e49aaec80e3
Thomas Basham <Bashamtg@gmail.com>
2022-02-14 16:45:16 -0800

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -1,50 +1,63 @@
 'use strict';
-console.log(`hello world`);
-// let cities = document.getElementById('kitten-profiles');
-
-// objects/cities
+console.log('hello world');
+let minMax = document.getElementById(minCustomers, maxCustomers);
+Let 
+let minCustomers = 0;
+let maxCustomers = 0;
+// objects/cities  seattle tokyo dubai paris lima
 let seattle = {
-  name: "Seattle",
+  name: 'Seattle',
   minCustomers:23,
   maxCustomers:65,
   avgCookiePerSale:6.3,
-}
+};
 
 let tokyo = {
-  name: "Tokyo",
+  name: 'Tokyo',
   minCustomers:3,
   maxCustomers:24,
   avgCookiePerSale:1.2,
-}
+};
 
 let dubai = {
-  name: "Dubai",
+  name: 'Dubai',
   minCustomers:11,
   maxCustomers:38,
   avgCookiePerSale:3.7,
-}
+};
 
-let Paris = {
-  name: "Paris",
+let paris = {
+  name: 'Paris',
   minCustomers:20,
   maxCustomers:38,
   avgCookiePerSale:2.3,
-}
+};
 
-let Lima = {
-  name: "Lima",
+let lima = {
+  name: 'Lima',
   minCustomers:2,
   maxCustomers:16,
   avgCookiePerSale:4.6,
-}
+};
+
+Lima.render = function() {
+  let h2elm = document.createElement('h2');
+  minMax.appendChild(h2elm);
+};
+console.log(Lima.maxCustomers);
 
 
+seattle ();
+tokyo ();
+dubai ();
+paris ();
+lima ();
 
 // function/method for generating random number of customers per hour
 function randomCookieNumber(minCustomers,maxCustomers){
   return Math.floor(Math.random() * (maxCustomers- minCustomers + 1) + minCustomers);
 }
-
+console.log(randomCookieNumber(1,10));
 
 // ********* DOM MANIPULATION ***********
 
