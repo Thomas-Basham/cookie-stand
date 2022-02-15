d9dfb0fb1d711b0b4703910e919b3bf6b3ea3fdb
Thomas Basham <Bashamtg@gmail.com>
2022-02-14 19:00:56 -0800

    worked on js

--- JS/app.js
+++ JS/app.js
@@ -1,63 +1,103 @@
 'use strict';
 console.log('hello world');
-let minMax = document.getElementById(minCustomers, maxCustomers);
-Let 
-let minCustomers = 0;
-let maxCustomers = 0;
-// objects/cities  seattle tokyo dubai paris lima
-let seattle = {
-  name: 'Seattle',
-  minCustomers:23,
-  maxCustomers:65,
-  avgCookiePerSale:6.3,
-};
 
-let tokyo = {
-  name: 'Tokyo',
-  minCustomers:3,
-  maxCustomers:24,
-  avgCookiePerSale:1.2,
-};
+// minMax();
 
-let dubai = {
-  name: 'Dubai',
-  minCustomers:11,
-  maxCustomers:38,
-  avgCookiePerSale:3.7,
-};
 
-let paris = {
-  name: 'Paris',
-  minCustomers:20,
-  maxCustomers:38,
-  avgCookiePerSale:2.3,
-};
+
+
+let salmonCookies = document.getElementById('avg-cookies');
+
+// let seattle = {
+//   name: 'Seattle',
+//   minCustomers:23,
+//   maxCustomers:65,
+//   avgCookiePerSale:6.3,
+// };
+
+// let tokyo = {
+//   name: 'Tokyo',
+//   minCustomers:3,
+//   maxCustomers:24,
+//   avgCookiePerSale:1.2,
+// };
+
+// let dubai = {
+//   name: 'Dubai',
+//   minCustomers:11,
+//   maxCustomers:38,
+//   avgCookiePerSale:3.7,
+// };
+
+// let paris = {
+//   name: 'Paris',
+//   minCustomers:20,
+//   maxCustomers:38,
+//   avgCookiePerSale:2.3,
+// };
+let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm',' 2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
 
 let lima = {
   name: 'Lima',
   minCustomers:2,
   maxCustomers:16,
   avgCookiePerSale:4.6,
+  cookiePerHour: hours.push[15],
+  getcookiePerHour: function(){
+    this.cookiePerHour = randomCookieNumber(lima.minCustomers, lima.maxCustomers);
+  }
 };
 
-Lima.render = function() {
-  let h2elm = document.createElement('h2');
-  minMax.appendChild(h2elm);
-};
-console.log(Lima.maxCustomers);
-
-
-seattle ();
-tokyo ();
-dubai ();
-paris ();
-lima ();
+// lima.getCookiePerHour();
+console.log(lima.avgCookiePerSale);
 
 // function/method for generating random number of customers per hour
-function randomCookieNumber(minCustomers,maxCustomers){
-  return Math.floor(Math.random() * (maxCustomers- minCustomers + 1) + minCustomers);
+function randomCookieNumber(min,max){
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
-console.log(randomCookieNumber(1,10));
+
+randomCookieNumber();
+// console.log(randomCookieNumber(1,10));
+
+
+lima.render = function() {
+  let h1elm = document.createElement('h1');
+
+  salmonCookies.appendChild(h1elm);
+};
+
+lima.render();
+
+
+let paraElem = document.createElement('p');
+paraElem.textContent = `${lima.minCustomers} and ${lima.maxCustomers}.`;
+salmonCookies.appendChild(paraElem);
+
+let ulElem = document.createElement('ul');
+salmonCookies.appendChild(ulElem);
+
+// for(let i = 0; i < this.hours.length; i++){
+//   let currentHours = this.hours[i];
+//   let liElem = document.createElement('li');
+//   liElem.textContent = currentHours;
+//   ulElem.appendChild(liElem);
+// }
+
+// need a loop that will return 14 random numbers from randomCookieNumber Function according to it's parent object's min/max customers.
+// neeed the total of those numbers added.
+
+
+
+
+
+
+// seattle();
+// tokyo();
+// dubai ();
+// paris ();
+// lima ();
+
 
 // ********* DOM MANIPULATION ***********
 
